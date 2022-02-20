3d12f992be1310c97cc996c5f3806717b88770c5
Jon Winsley <jon.winsley@gmail.com>
2022-02-20 21:24:08 +0000

    Importing wordlist Checking for valid words

--- src/words.rs
+++ src/words.rs
@@ -1,34 +1,16 @@
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 use rand::{thread_rng, Rng};
 
-pub fn get_word(index: &u32) -> Result<String, String> {
-    let mut counter = 0;
-    if let Ok(lines) = read_lines("resources/words.txt") {
-        for line in lines {
-            if index == &counter {
-                if let Ok(i) = line {
-                    return Ok(i);
-                } else {
-                    return Err("Unable to load wordfile".to_string());
-                }
-            }
-            counter += 1;
-        }
-        return Err("Index greater than wordlist length".to_string());
-    }
-    Err("Unable to load wordfile".to_string())
+static LINES: &str = include_str!("words.txt");
+
+pub fn get_word(index: usize) -> Option<&'static str> {
+    LINES.lines().nth(index)
 }
 
-pub fn get_random_word() -> Result<u32, String> {
+pub fn is_valid_word(word: &str) -> bool {
+    LINES.lines().any(|dict_word| dict_word == word)
+}
+
+pub fn get_random_word() -> Result<usize, String> {
     let mut rng = thread_rng();
-    let lines = read_lines("resources/words.txt").map_err(|_| "Unable to load wordfile".to_string())?;
-    Ok(rng.gen_range(0..lines.count()).try_into().map_err(|_| "Failed to generate word")?)
-}
-
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+    Ok(rng.gen_range(0..LINES.lines().count()))
 }
